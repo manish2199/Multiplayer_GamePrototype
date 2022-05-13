8f764dcb35e3b5b6e402461544ccd792d2b2e666
Manish <manishmistry9799@gmail.com>
2022-05-13 15:27:13 +0530

    Setup Photon And MainMenuScene

---
+++ Assets/Scripts/Connect To Server/ConnectToServer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    public InputField userNameInput;
+    public Text buttonText;
+
+    public void OnClickConnect()
+    {
+        if(userNameInput.text.Length >= 1)
+       {
+           PhotonNetwork.NickName = userNameInput.text;
+           buttonText.text = "Connecting...";
+           PhotonNetwork.ConnectUsingSettings();
+       }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+      SceneManager.LoadScene("Lobby");
+    }
+ 
+}
